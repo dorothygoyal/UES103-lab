9d8b9d2f0242dc5c7eec8f10e9aef60188146478
dorothygoyal <dorothyygoyal@gmail.com>
2025-09-15 01:01:59 +0530

    Add files via upload

---
+++ ques5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//name-dorothy
+//group-1a32
+//date-1sep
+//roll no.-1025030170
+
+int main() {
+	char name='dorothy';
+	int rollno=1025030170;
+	printf("hello world") ;
+	return 0 ;
+
+}
