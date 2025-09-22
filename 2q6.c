0deee075757cfeb20766e16f75212be0c8ae07ae
dorothygoyal <dorothyygoyal@gmail.com>
2025-09-23 03:34:22 +0530

    Add files via upload

---
+++ 2q6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//name-dorothy
+//group-1a32
+//date-22sep
+//roll no-1025030170
+
+int main() {
+	char const name="dorothy";
+	int rollno = 1025030170;
+	int a;
+	printf("enter number");
+	scanf("%d",&a);
+	int b;
+	b=a>>2;
+	printf("%d",b);
+}
