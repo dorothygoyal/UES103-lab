3c27301a20d85f97d3267ee32deb3febad5bd234
dorothygoyal <dorothyygoyal@gmail.com>
2025-09-23 13:18:47 +0530

    Add files via upload

---
+++ 3q11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//name-dorothy
+//group-1a32
+//date-14sep
+//roll no.-1025030170
+
+int main() {
+	char name='dorothy';
+	int rollno=1025030170;
+	int x;
+	while (x<100) {
+	printf("enter num");
+	scanf("%d",&x);
+	if (x>100) {
+	printf("QUIT");
+}}}
